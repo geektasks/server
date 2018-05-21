207e296b20eeda880d3f3c4f27ca0997e1cfd0f7
pilik <pilikinp@me.com>
2018-05-21 17:26:12 +0300

    набросок сервера, пока только принимает и отсылает сообщение обратно, т.к. нет структуры бд

---
+++ serv/create_config.py
@@ -0,0 +1,30 @@
+import os
+import configparser
+
+
+def create_config(path):
+
+    config = configparser.ConfigParser()
+    config.add_section("Settings")
+    config.set("Settings", "ip", "127.0.0.1")
+    config.set("Settings", "port", "7777")
+
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+def get_config(path):
+    if not os.path.exists(path):
+        create_config(path)
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+
+def get_setting(path, section, setting):
+    config = get_config(path)
+    value = config.get(section, setting)
+    return value
+
+if __name__ == "__main__":
+    path = "settings.ini"
+    create_config(path)
