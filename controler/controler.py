7a4c180e1a8cb3e50e00a298458e78012b7950aa
pilik <pilikinp@me.com>
2018-05-24 21:11:58 +0300

    пробная регистрация, надо продумать как формировать словарь transport, что то у меня пока мыслей нет

--- controler/controler.py
+++ controler/controler.py
@@ -1,4 +1,4 @@
-from controler.registration import registration
+from controler.registration import registration, check_user
 from controler.authorization import authorization
 import serv.shortcuts as shortcuts
 
@@ -8,19 +8,20 @@ TYPE = {
 }
 NAME ={
 	'registration':registration,
-	'authorization':authorization
+	'authorization':authorization,
+    'check_user': check_user
 }
 
 class CControler:
 
     @classmethod
-    def handle(cls,request):
+    def handle(cls, request):
         try:
             head = request['head']
             head_type= head['type']
             head_name=head['name']
             if head_type in TYPE and head_name in NAME:
-                body=request('body')
+                body=request['body']
                 controller = NAME.get(head_name)
                 return controller(body)
             else:
@@ -29,7 +30,4 @@ class CControler:
         except Exception as err:
             print(err)
             return shortcuts.internal_server_error
-
-
-
 
--- controler/registration.py
+++ controler/registration.py
@@ -1,3 +1,56 @@
+from repository.repository import Repository, Users
 
-def registration(request):
-    pass
+serverdb = Repository()
+
+def registration(body):
+    global client_dict
+    print(body)
+    if serverdb.add(Users(body['name'], body['password'])):
+        message = {
+            "head": {
+                "type": "server response",
+                "name": "registration"
+            },
+            "body": {
+                "code": 201,
+                "message": "created"
+            }
+        }
+    else:
+        message = {
+            "head": {
+                "type": "server response",
+                "name": "registration error"
+            },
+            "body": {
+                "code": 500,
+                "message": "Error"
+            }
+        }
+    return message
+
+def check_user(body):
+    if serverdb.get_user(body['name']):
+        message = {
+            "head": {
+                "type": "server response",
+                "name": "check_user"
+            },
+            "body": {
+                "code": 409,
+                "message": "error"
+            }
+        }
+        return message
+    else:
+        message = {
+            "head": {
+                "type": "server response",
+                "name": "check_user"
+            },
+            "body": {
+                "code": 200,
+                "message": "ok"
+            }
+        }
+        return message
