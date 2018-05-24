3b35aa660e7cec2b8a12055d8a44c8a49edc2b74
pilik <pilikinp@me.com>
2018-05-24 18:07:50 +0300

    заготовка для проверки есть ли пользователь в бд

--- controler/controler.py
+++ controler/controler.py
@@ -1,8 +1,18 @@
+from repository.repository import Repository
+
+servdb = Repository()
+
+def msg_in(request):
+	print(request)
+	print(request['action'])
+	result = commands[request['action']](request['data'])
+	return result
+
+def check_user(data):
+    if servdb.get_user(data):
+        return {'No':'no'}
+    else:
+        return {'ok': 'ok'}
 
 
-
-
-
-def msg_in(reqest)
-	print(reqest)
-	return(reqest)
+commands = {'check_user': check_user}
