e3c38564dde46cae838378671bf018fcd221372a
glygs@mail.ru <glygs@mail.ru>
2018-05-28 14:50:54 +0500

    вывод консоли перенаправлен в файлы. log_err и log

--- run_serv.py
+++ run_serv.py
@@ -1,21 +1,34 @@
 import asyncio
 from serv.async_serv import ServerClientProtocol
 
-from serv.create_config import get_setting
-path = 'serv/settings.ini'
-ip = get_setting(path, 'Settings', 'ip')
-port = get_setting(path, 'Settings', 'port')
-
-loop = asyncio.get_event_loop()
-coro = loop.create_server(ServerClientProtocol, ip, port)
-server = loop.run_until_complete(coro)
-
-print('Serving on {}'.format(server.sockets[0].getsockname()))
+import sys
 try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    sys.stdout=open("log.txt","w")
+    sys.stderr=open("log_err.txt","w")
 
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+
+    from serv.create_config import get_setting
+    path = 'serv/settings.ini'
+    ip = get_setting(path, 'Settings', 'ip')
+    port = get_setting(path, 'Settings', 'port')
+
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(ServerClientProtocol, ip, port)
+    server = loop.run_until_complete(coro)
+
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
+    sys.stdout.close()
+    sys.stderr.close()
+except Exception as err:
+    print(err)
+    loop.close()
+    sys.stdout.close()
+    sys.stderr.close()
