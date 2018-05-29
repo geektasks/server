c187df55ea35f0780f027dd27ea8cb95dcac5c6d
Pegorino82 <eshkryabin@yandex.ru>
2018-05-29 21:06:56 +0300

    несущественные правки

--- repository/models_users.py
+++ repository/models_users.py
@@ -4,19 +4,20 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from repository.db_core import CBase
 
+
 class Users(CBase):
-
     __tablename__ = 'users'
-    user_id = Column(INTEGER(), primary_key = True,autoincrement=True)
-    username = Column(VARCHAR(45), nullable= False, unique= True)
-    password = Column(LONGTEXT, nullable= False)
-    email = Column(VARCHAR(45), nullable= True)
-    session_id = Column(INTEGER())
+    user_id = Column(INTEGER(), primary_key=True, autoincrement=True)
+    username = Column(VARCHAR(45), nullable=False, unique=True)
+    password = Column(LONGTEXT, nullable=False)
+    email = Column(VARCHAR(45), nullable=True)
+    session_id = Column(INTEGER())  # TODO сделать поле уникальным для идентификации пользователя
 
-    def __init__(self, username, password, email = None, session_id=None):
+    def __init__(self, username, password, email=None, session_id=None):
         self.username = username
         self.password = password
         self.email = email
-        self.session_id= session_id
+        self.session_id = session_id
+
     def __repr__(self):
         return self.username
