fbf3daddd03661ae30afbd5e89766b3c066a06f5
Pegorino82 <eshkryabin@yandex.ru>
2018-05-29 21:09:30 +0300

    опечатки, ctrl+alt+L

--- controler/authorization.py
+++ controler/authorization.py
@@ -1,18 +1,17 @@
 from repository.repository import Repository, Users
-from serv.shortcuts import auth_done, auth_err,internal_server_error
+from serv.shortcuts import auth_done, auth_err, internal_server_error
 from random import randrange
 
 serverdb = Repository()
 
+
 def authorization(body):
     print(body)
-    if serverdb.get_pass(body['name']==body['password']):
-        sesion_id=randrange(1000000000,99999999999)
-        if serverdb.set_session_id(body['name'],randrange(1000000000,99999999999)):
-            return auth_done(sesion_id)
+    if serverdb.get_pass(body['name'] == body['password']):
+        session_id = randrange(1000000000, 99999999999)
+        if serverdb.set_session_id(body['name'], randrange(1000000000, 99999999999)):
+            return auth_done(session_id)
         else:
             return internal_server_error
     else:
         return auth_err
-
-
