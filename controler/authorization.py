b2d668773c46bf22300db497c327c6c6af4d81f9
glygs@mail.ru <glygs@mail.ru>
2018-05-30 15:25:34 +0500

    Поправлен баг
    Signed-off-by: glygs@mail.ru <glygs@mail.ru>

--- controler/authorization.py
+++ controler/authorization.py
@@ -6,7 +6,7 @@ serverdb = Repository()
 
 def authorization(body):
     print(body)
-    if serverdb.get_pass(body['name']==body['password']):
+    if serverdb.get_pass(body['name'])==body['password']:
         sesion_id=randrange(1000000000,99999999999)
         if serverdb.set_session_id(body['name'],randrange(1000000000,99999999999)):
             return auth_done(sesion_id)
