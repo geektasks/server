20d5a456a65bc2222380a88b45c0f765b693ca79
Tanya Ryzhova <rtanyas@gmail.com>
2018-06-02 15:52:56 +0300

    Revert "+ server answers on tasks;"
    This reverts commit ef8ac302d8843c28e52a3e2c6b86dc0db5598ce4.

--- serv/shortcuts.py
+++ serv/shortcuts.py
@@ -3,25 +3,23 @@ unknown_request = {
         "type": "error",
         "name": "bad_request"
     },
-    "body": {
+    "body":{
         "code": 400,
         "message": "unknown_request"
     }
 }
 
-
 def internal_server_error(err='internal_server_error'):
     return {
-        "head": {
-            "type": "error",
-            "name": "internal_server_error"
-        },
-        "body": {
-            "code": 400,
-            "message": '%s' % (err)
-        }
+    "head": {
+        "type": "error",
+        "name": "internal_server_error"
+    },
+    "body":{
+        "code": 400,
+        "message": '%s'%(err)
     }
-
+}
 
 user_created = {
     "head": {
@@ -35,15 +33,15 @@ user_created = {
 }
 
 reg_error = {
-    "head": {
-        "type": "server response",
-        "name": "registration error"
-    },
-    "body": {
-        "code": 500,
-        "message": "Error"
-    }
-}
+            "head": {
+                "type": "server response",
+                "name": "registration error"
+            },
+            "body": {
+                "code": 500,
+                "message": "Error"
+            }
+        }
 
 check_user_err = {
     "head": {
@@ -57,106 +55,44 @@ check_user_err = {
 }
 
 check_user_ok = {
-    "head": {
-        "type": "server response",
-        "name": "check_user"
-    },
-    "body": {
-        "code": 200,
-        "message": "ok"
-    }
-}
-
-
+            "head": {
+                "type": "server response",
+                "name": "check_user"
+            },
+            "body": {
+                "code": 200,
+                "message": "ok"
+            }
+        }
 def auth_done(session_id):
     return {
-        "head": {
-            "type": "server response",
-            "name": "authorization"
-        },
-        "body": {
-            "code": 200,
-            "message": '%i' % (session_id)
-        }
+    "head": {
+        "type": "server response",
+        "name": "authorization"
+    },
+    "body":{
+        "code": 200,
+        "message": '%i'%(session_id)
     }
-
-
+}
 auth_err = {
     "head": {
         "type": "server response",
         "name": "authorization"
     },
-    "body": {
+    "body":{
         "code": 401,
         "message": "unauthorized"
     }
 }
 
-hello = {
-    'head': {
-        'type': 'Hello_from_server',
-        'name': 'online'
-    },
-    'body': {
-        'Code': '',
-        'message': ''
-    }
-}
-
-
-def task_created(task_id):
-    return {
-        "head": {
-            "type": "server response",
-            "name": "create task"
+hello= {
+    'head':{
+        'type':'Hello_from_server',
+        'name':'online'
         },
-        "body":{
-            "code": 201,
-            "message": "сreated",
-            "id": '%i'%(task_id)
+    'body':{
+        'Code':'',
+        'message':''
         }
     }
-
-task_unauth = {
-    "head": {
-        "type": "server response",
-        "name": "create task"
-    },
-    "body":{
-        "code": 401,
-        "message": "unauthorized"
-    }
-}
-
-task_edit = {
-    "head": {
-        "type": "server response",
-        "name": "edit task"
-    },
-    "body":{
-        "code": 200,
-        "message": "ok"
-    }
-}
-
-task_not_found = {
-    "head": {
-        "type": "server response",
-        "name": "edit task"
-    },
-    "body":{
-        "code": 400,
-        "message": "bad request"
-    }
-}
-
-task_no_permission = {
-    "head": {
-        "type": "server response",
-        "name": "edit task"
-    },
-    "body":{
-        "code": 403,
-        "message": "forbidden"
-    }
-}
