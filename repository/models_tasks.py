906932b6cca77d9fc66e59930686b3be89aef454
Pegorino82 <eshkryabin@yandex.ru>
2018-06-24 13:47:17 +0300

    добавил новые поля (дата/время)

--- repository/models_tasks.py
+++ repository/models_tasks.py
@@ -5,6 +5,7 @@ from repository.models_users import Users
 
 from repository.db_core import CBase
 
+
 # TODO добавить поля time / deadline
 class Tasks(CBase):
     __tablename__ = 'tasks'
@@ -13,14 +14,23 @@ class Tasks(CBase):
     description = Column(LONGTEXT, nullable=True)
     status = Column(INTEGER(), nullable=False)
     creator_id = Column(INTEGER(), ForeignKey('users.user_id'), nullable=False)
+    date_create = Column(VARCHAR(25))
+    date_deadline = Column(VARCHAR(25))
+    date_reminder = Column(VARCHAR(25))
+    time_reminder = Column(VARCHAR(25))
 
     creator = relationship(Users, foreign_keys=[creator_id])
 
-    def __init__(self, creator_id, name, status=0, description=None):
+    def __init__(self, creator_id, name, status=0, description=None, date_create=None, date_deadline=None,
+                 date_reminder=None, time_reminder=None):
         self.creator_id = creator_id
         self.name = name
         self.status = status
         self.description = description
+        self.date_create = date_create
+        self.date_deadline = date_deadline
+        self.date_reminder = date_reminder
+        self.time_reminder = time_reminder
 
     def __repr__(self):
         return '{} {}'.format(self.name, self.description)
